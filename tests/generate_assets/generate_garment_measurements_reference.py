fc14c75ddc828b3f344c6684605be1c36c65082c
Andrea Boscolo Camiletto <abcamiletto@gmail.com>
2026-04-24 01:40:34 -0700

    Pin GarmentMeasurements generator NumPy below 2

--- tests/generate_assets/generate_garment_measurements_reference.py
+++ tests/generate_assets/generate_garment_measurements_reference.py
@@ -2,7 +2,7 @@
 # requires-python = ">=3.11,<3.12"
 # dependencies = [
 #   "bpy>=4.2.0",
-#   "numpy>=1.26",
+#   "numpy>=1.26,<2",
 # ]
 # ///
 """Generate the private GarmentMeasurements rig asset from upstream data.
