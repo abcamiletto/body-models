547bd1260be0c8f82598187b00c78ba1ed75eb81
Andrea Boscolo Camiletto <abcamiletto@gmail.com>
2026-05-11 08:47:18 -0700

    expand standard hand joint names

--- src/body_models/brainco/constants.py
+++ src/body_models/brainco/constants.py
@@ -3,20 +3,32 @@ from body_models.constants import Joint
 
 LEFT_BRAINCO_JOINTS = {
     Joint.LEFT_WRIST: "left_base_skel",
-    Joint.LEFT_THUMB_TIP: "left_thumb_distal_skel",
-    Joint.LEFT_INDEX_TIP: "left_index_distal_skel",
-    Joint.LEFT_MIDDLE_TIP: "left_middle_distal_skel",
-    Joint.LEFT_RING_TIP: "left_ring_distal_skel",
-    Joint.LEFT_PINKY_TIP: "left_pinky_distal_skel",
+    Joint.LEFT_THUMB_CMC: "left_thumb_metacarpal_skel",
+    Joint.LEFT_THUMB_MCP: "left_thumb_proximal_skel",
+    Joint.LEFT_THUMB_IP: "left_thumb_distal_skel",
+    Joint.LEFT_INDEX_MCP: "left_index_proximal_skel",
+    Joint.LEFT_INDEX_DIP: "left_index_distal_skel",
+    Joint.LEFT_MIDDLE_MCP: "left_middle_proximal_skel",
+    Joint.LEFT_MIDDLE_DIP: "left_middle_distal_skel",
+    Joint.LEFT_RING_MCP: "left_ring_proximal_skel",
+    Joint.LEFT_RING_DIP: "left_ring_distal_skel",
+    Joint.LEFT_PINKY_MCP: "left_pinky_proximal_skel",
+    Joint.LEFT_PINKY_DIP: "left_pinky_distal_skel",
 }
 
 RIGHT_BRAINCO_JOINTS = {
     Joint.RIGHT_WRIST: "right_base_skel",
-    Joint.RIGHT_THUMB_TIP: "right_thumb_distal_skel",
-    Joint.RIGHT_INDEX_TIP: "right_index_distal_skel",
-    Joint.RIGHT_MIDDLE_TIP: "right_middle_distal_skel",
-    Joint.RIGHT_RING_TIP: "right_ring_distal_skel",
-    Joint.RIGHT_PINKY_TIP: "right_pinky_distal_skel",
+    Joint.RIGHT_THUMB_CMC: "right_thumb_metacarpal_skel",
+    Joint.RIGHT_THUMB_MCP: "right_thumb_proximal_skel",
+    Joint.RIGHT_THUMB_IP: "right_thumb_distal_skel",
+    Joint.RIGHT_INDEX_MCP: "right_index_proximal_skel",
+    Joint.RIGHT_INDEX_DIP: "right_index_distal_skel",
+    Joint.RIGHT_MIDDLE_MCP: "right_middle_proximal_skel",
+    Joint.RIGHT_MIDDLE_DIP: "right_middle_distal_skel",
+    Joint.RIGHT_RING_MCP: "right_ring_proximal_skel",
+    Joint.RIGHT_RING_DIP: "right_ring_distal_skel",
+    Joint.RIGHT_PINKY_MCP: "right_pinky_proximal_skel",
+    Joint.RIGHT_PINKY_DIP: "right_pinky_distal_skel",
 }
 
 
--- src/body_models/brainco/torch.py
+++ src/body_models/brainco/torch.py
@@ -53,7 +53,7 @@ class BrainCoHand(BodyModel, nn.Module):
         return self.weights.joint_names
 
     @property
-    def _standard_joints(self):
+    def common_joints(self):
         return LEFT_BRAINCO_JOINTS if self.side == "left" else RIGHT_BRAINCO_JOINTS
 
     @property
