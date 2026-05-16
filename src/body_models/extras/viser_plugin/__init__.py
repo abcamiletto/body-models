b59c5f31a0d51e112384d6e67d25420d6e0d83d2
Andrea Boscolo Camiletto <aboscolo@mpi-inf.mpg.de>
2026-05-17 00:22:23 +0200

    Add viser rigid body extras

--- src/body_models/extras/viser_plugin/__init__.py
+++ src/body_models/extras/viser_plugin/__init__.py
@@ -1,11 +1,14 @@
 """Optional ``viser`` helpers for body model visualization."""
 
 from body_models.extras.viser_plugin.body_model import ViserBodyModelHandle, add_body_model
+from body_models.extras.viser_plugin.rigid_body import ViserRigidBodyModelHandle, add_rigid_body_model
 from body_models.extras.viser_plugin.skeleton import ViserSkeletonHandle, add_skeleton
 
 __all__ = [
     "ViserBodyModelHandle",
+    "ViserRigidBodyModelHandle",
     "ViserSkeletonHandle",
     "add_body_model",
+    "add_rigid_body_model",
     "add_skeleton",
 ]
