2a9bcdddb571f4ff2fda28e58d4c504fb0deb8ee
Andrea Boscolo Camiletto <aboscolo@mpi-inf.mpg.de>
2026-05-17 00:38:47 +0200

    Add viser skeleton extras (#101)

---
+++ src/body_models/extras/viser_plugin/__init__.py
@@ -0,0 +1,8 @@
+"""Optional ``viser`` helpers for body model visualization."""
+
+from body_models.extras.viser_plugin.skeleton import ViserSkeletonHandle, add_skeleton
+
+__all__ = [
+    "ViserSkeletonHandle",
+    "add_skeleton",
+]
