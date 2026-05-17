d744e9247a7d9a9b1fc1ef13df26396afa0bb121
Andrea Boscolo Camiletto <aboscolo@mpi-inf.mpg.de>
2026-05-17 12:23:57 +0200

    Add optional viser extra

--- src/body_models/extras/viser_plugin/__init__.py
+++ src/body_models/extras/viser_plugin/__init__.py
@@ -1,5 +1,13 @@
 """Optional ``viser`` helpers for body model visualization."""
 
+from importlib.util import find_spec
+
+if find_spec("viser") is None:
+    raise ImportError(
+        "body_models.extras.viser_plugin requires the optional 'viser' dependency. "
+        "Install it with `pip install 'body-models[viser]'` or `uv add 'body-models[viser]'`."
+    )
+
 from body_models.extras.viser_plugin.body_model import ViserBodyModelHandle, add_body_model
 from body_models.extras.viser_plugin.rigid_body import ViserRigidBodyModelHandle, add_rigid_body_model
 from body_models.extras.viser_plugin.skeleton import ViserSkeletonHandle, add_skeleton
